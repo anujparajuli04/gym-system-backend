29065655d80aa02dff29a3924081ef375006821f
Anuj Parajuli <parajulianuj246@gmail.com>
2026-03-22 07:35:36 +0545

    Add update profile endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,29 @@
 const User = require("../models/User");
 
-// GET PROFILE (from DB)
 exports.getProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
-
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+    res.json({ message: "User profile fetched successfully", user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    res.json({
-      message: "User profile fetched successfully",
-      user
-    });
-
+exports.updateProfile = async (req, res) => {
+  try {
+    const { name, phone } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { name, phone },
+      { new: true }
+    ).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ message: "Profile updated successfully", user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
